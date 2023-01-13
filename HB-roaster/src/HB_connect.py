c12c5ba53d7ba1f8b2a4fec712f2a3450301e59d
Tim <sakunamary@gmail.com>
2023-01-13 16:59:14 +0800

    Update HB_connect.py

--- HB-roaster/src/HB_connect.py
+++ HB-roaster/src/HB_connect.py
@@ -1,9 +1,13 @@
+#!/usr/bin/python
+
+
 #库引用
 import asyncio
 
 import time,serial,json,socket
 # import websockets #pip3 install websockets
 # import  websockets
+# https://geek-docs.com/python/python-tutorial/python-simplejson.html
 import websockets
 
 #全局变量定义
@@ -11,20 +15,13 @@ import websockets
 # serial_port = '/dev/cu.usbmodem00001'
 serial_port = '/dev/cu.usbserial-14310'
 buadrate = 57600
-rl = ''
-res1300 = ''
-res2400 = ''
-bt = 0.0
-et = 0.0 
-drum = 0.0
-Air  = 0.0
-IP_ADD = ''
 
-command = ''
 #对象声明
 #声明串口
 serial_port = serial.Serial(serial_port,buadrate,8,'N',1,timeout=0.5)
-#声明WS client
+
+#构建 json 的dict类
+
 
 
 # 函数定义
@@ -47,6 +44,17 @@ def get_host_ip():
         return ip
 
 def get_tempeture():
+    RESULT_LINE = ''
+    res1300 = ''
+    res2400 = ''
+    bt = 0.0
+    ET = 0.0
+    drum = 0.0
+    Air  = 0.0
+    command = ''
+    port_ok_flag = serial_port.isOpen()
+    print("port is open:",port_ok_flag)
+
     if (port_ok_flag) :
                 #print("port_ok_flag:",port_ok_flag)
 
@@ -54,7 +62,7 @@ def get_tempeture():
         serial_port.write(str2cmd(command))
         serial_port.flush()
         time.sleep(0.3)
-        rl = serial_port.readline().decode('utf-8', 'ignore')[:-2]
+        RESULT_LINE = serial_port.readline().decode('utf-8', 'ignore')[:-2]
         # if (rl.startswith('#')):
         #     print("CHAN 1300:",rl)
 
@@ -62,10 +70,10 @@ def get_tempeture():
         serial_port.write(str2cmd(command))
         time.sleep(0.2)
         serial_port.flush()
-        rl = serial_port.readline().decode('utf-8', 'ignore')[:-2]
-        if (not len(rl) == 0 and not rl.startswith('#')):
+        RESULT_LINE = serial_port.readline().decode('utf-8', 'ignore')[:-2]
+        if (not len(RESULT_LINE) == 0 and not RESULT_LINE.startswith('#')):
             # print("READ 1300:",rl)
-            res1300 = rl.rsplit(',')
+            res1300 = RESULT_LINE.rsplit(',')
             bt = float(res1300[0])
             Air =float(res1300[1])
 
@@ -73,7 +81,7 @@ def get_tempeture():
         serial_port.write(str2cmd(command))
         serial_port.flush()
         time.sleep(0.3)
-        rl = serial_port.readline().decode('utf-8', 'ignore')[:-2]
+        RESULT_LINE = serial_port.readline().decode('utf-8', 'ignore')[:-2]
         # if (rl.startswith('#')):
         #     print("CHAN 2400:",rl)
 
@@ -81,27 +89,29 @@ def get_tempeture():
         serial_port.write(str2cmd(command))
         time.sleep(0.2)
         serial_port.flush()
-        rl = serial_port.readline().decode('utf-8', 'ignore')[:-2]
-        if (not len(rl) == 0 and not rl.startswith('#')):
+        RESULT_LINE = serial_port.readline().decode('utf-8', 'ignore')[:-2]
+        if (not len(RESULT_LINE) == 0 and not RESULT_LINE.startswith('#')):
             # print("READ 2400: ",rl)
-            res2400 = rl.rsplit(',')
-            et = float(res2400[0])
+            res2400 = RESULT_LINE.rsplit(',')
+            ET = float(res2400[0])
             drum =float(res2400[1])
+
+        send_back = json.dumps
+        return send_back              
         
-        print("BT:",bt,"ET:",bt,"AIR:",Air,"DURM:",drum)
-    #asyncio.run(main())
-
-
-async def echo(websocket):
-    async for message in websocket:
-        await websocket.send(message)
+        # print("BT:",bt,"ET:",bt,"AIR:",Air,"DURM:",drum)
 
 
 async def handler(websocket, path):
     while True:
         message = await websocket.recv()
-        get_tempeture() # 获取一次温度数据
+        data = json.loads(message)  #  {"command": "getData", "id": 35632, "roasterID": 0}
+        if data['command'] == 'getData':
+            #print(data['command'])
+            get_tempeture() # 获取一次温度数据
+            send_back_json = get_tempeture()
         print(message)
+        print(send_back_json)
  
 async def main():
     async with websockets.serve(handler, "", 8080):
@@ -113,9 +123,6 @@ async def main():
 
 
 #主程序
-
-port_ok_flag = serial_port.isOpen()
-
 print(get_host_ip()) #显示本机IP地址
 
 asyncio.run(main())
