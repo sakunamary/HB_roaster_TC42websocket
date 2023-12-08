cd2957e060c277cb701e564dee7b5ddb9b6c5322
Tim <sakunamary@gmail.com>
2023-12-08 09:39:49 +0800

    Update config.h

--- HB_roaster_test/src/config.h
+++ HB_roaster_test/src/config.h
@@ -7,8 +7,25 @@
 #define BAUDRATE 115200  //serial port baudrate
 
 
-
 #define VERSION "1.0.5"
+
+
+
+#define DEBUG_MODE
+//#define TXD 16
+//#define RXD 17
+
+//pinout setting
+#define ENC_CLK  33
+#define ENC_DT   32
+#define PWM_HEAT 26
+
+
+
+
+//pwm setting 
+#define PWM_FREQ 10000
+#define PWM_RESOLUTION 10 //0-1024
 
 // 网页设置的参数
  typedef struct eeprom_settings 
